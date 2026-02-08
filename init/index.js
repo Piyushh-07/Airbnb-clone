55886882eee8de87f0baae0222f7830dd1ff95c3
Piyush <piyush840056@gmail.com>
2026-02-09 01:16:47 +0530

    sample data created

---
+++ init/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../model/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+
+main()
+.then(()=>{
+    console.log("connected to the database")
+}).catch((err)=>{
+    console.log(err)
+})
+
+async function main(){
+    await mongoose.connect(MONGO_URL)
+}
+
+const initDB = async()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("database initialized with sample data");
+}
+initDB();
