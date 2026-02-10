a861f31f982f432384e5e62001ca87351bbce6ce
Piyush <piyush840056@gmail.com>
2026-02-11 03:04:28 +0530

    fixing some error and set ejs file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./model/listing.js")
+const Listing = require("./model/listings.js")
+const path=require("path")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 
@@ -15,23 +16,31 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL)
 }
+app.set("view engine","ejs")
+app.set("views", path.join(__dirname,"views"))
 
 app.get("/",(req,res)=>{
     res.send("hi, I am root")
 })
-app.get("/testlisting",async (req,res)=>{
-    let sampleListing = new Listing({
-        title:"My beautiful villa",
-        description:"This is a beautiful villa located in the heart of the city. It has 4 bedrooms, 3 bathrooms, and a spacious living room. The villa also features a private pool and a large garden.",
-        price:1700,
-        location:"goa",
-        country:"India",
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { listings: allListings });
 });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("sucessfull testing");
-});
+// app.get("/testlisting",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title:"My beautiful villa",
+//         description:"This is a beautiful villa located in the heart of the city. It has 4 bedrooms, 3 bathrooms, and a spacious living room. The villa also features a private pool and a large garden.",
+//         price:1700,
+//         location:"goa",
+//         country:"India",
+// });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("sucessfull testing");
+// });
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080")
