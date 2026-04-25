2bb655264922ef5465ebc6f7f6c5dc44b73bc4c6
Piyush <piyush840056@gmail.com>
2026-04-25 23:47:31 +0530

    owner fixed for every listings

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../model/listing.js");
+const Listing = require("../model/listings.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 
 main()
@@ -16,6 +16,10 @@ async function main(){
 
 const initDB = async()=>{
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+        ...obj, 
+        owner:"64b8c9e5f1a4c3e5d6a7b8c" 
+    }));
     await Listing.insertMany(initData.data);
     console.log("database initialized with sample data");
 }
