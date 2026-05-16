99301b2b75d5cbdbffa93c49bdae99e3fb31d4e0
Piyush <piyush840056@gmail.com>
2026-05-17 04:32:46 +0530

    fixed homepage

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ app.use((req, res, next) => {
 //     res.send(registeredUser)
 // })
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 
 app.use("/listings", listingsRoutes)
 app.use("/listings/:id/reviews", reviewsRoutes)
